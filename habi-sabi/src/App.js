7292a12297644fbd405ff3648737889b42e689ed
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-10-06 11:52:50 -0500

    add unsubscribe methods to app.js

--- habi-sabi/src/App.js
+++ habi-sabi/src/App.js
@@ -6,19 +6,42 @@ import ToolsPage from "./pages/toolspage/toolspage.component";
 import AboutPage from "./pages/aboutpage/aboutpage.component";
 import { Switch, Route } from "react-router-dom";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
+import { auth } from "./firebase/firebase.utils";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/tools" component={ToolsPage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/signin" component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChange((user) => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/tools" component={ToolsPage} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
